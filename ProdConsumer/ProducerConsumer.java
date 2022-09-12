8c836d3cae655e5c2e6d0ad5aed8c3533cdf7eb1
Lakshminarayanan <imlash@outlook.com>
2022-09-12 19:40:20 +0530

    initial commit as part of producerconsumer trails

--- ProdConsumer/ProducerConsumer.java
+++ ProdConsumer/ProducerConsumer.java
@@ -1,5 +1,43 @@
 package ProdConsumer;
 
+import java.sql.Time;
+import java.util.concurrent.ArrayBlockingQueue;
+
+class Producer implements Runnable {
+    ArrayBlockingQueue que;
+    Producer(ArrayBlockingQueue que){
+        this.que=que;
+    }
+    //produce messages and add to queue
+    @Override
+    public void run() {
+        String ThreadName = printThread();
+        que.add("message by "+ThreadName);
+    }
+    public String printThread(){
+        Thread t = Thread.currentThread();
+        return t.getName();
+    }
+}
+
+class Consumer implements Runnable {
+    ArrayBlockingQueue que;
+    Consumer(ArrayBlockingQueue que){
+        this.que=que;
+    }
+    //consume messages when the queue is not empty
+    @Override
+    public void run(){
+        String ThreadName = printThread();
+//        message = que.poll();
+        System.out.println(ThreadName+" consumed "+que.poll());
+    }
+    public String printThread(){
+        Thread t = Thread.currentThread();
+        return t.getName();
+    }
+}
+
 public class ProducerConsumer {
     /*bufferoverflow - when producer produces more than queue length - data will be lost
     Average rate of production < Average rate of consumption
@@ -10,10 +48,21 @@ public class ProducerConsumer {
     2.Prevent producers from trying to add data to a full queue
     3.Prevent consumers from trying to remove data from a empty queue/buffer
      */
-
-
-
-
-}
+    public static void main(String[] args) throws InterruptedException {
+        //using a blocking queue which is thread safe
+        ArrayBlockingQueue que = new ArrayBlockingQueue(5);
+        for (int i=0;i<4;i++){
+            Thread P1 = new Thread(new Producer(que));
+            P1.start();
+//            Thread.sleep(2000);
+        }
+        System.out.println(que);
+        for (int i=0;i<5;i++){
+            Thread C1 = new Thread(new Consumer(que));
+            C1.start();
+//            Thread.sleep(2000);
+        }
+        System.out.println(que);
+    }
 
 }
