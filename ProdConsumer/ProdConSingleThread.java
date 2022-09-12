df4a0b5328d1ee34cae747a97e4c98144bc7579e
Lakshminarayanan <imlash@outlook.com>
2022-09-12 19:43:23 +0530

    prodconsumer single thread queue handling learning

--- ProdConsumer/ProdConSingleThread.java
+++ ProdConsumer/ProdConSingleThread.java
@@ -1,2 +1,56 @@
-package ProdConsumer;public class ProdConSingleThread {
+package ProdConsumer;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+class Prod implements Runnable {
+    ArrayBlockingQueue que;
+    Prod(ArrayBlockingQueue que){
+        this.que=que;
+    }
+    //produce messages and add to queue
+    @Override
+    public void run() {
+        try{
+            for (int i=0;i<10;i++) {
+                System.out.println(que.add("message by Prod" + i));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
+
+class Cons implements Runnable {
+    ArrayBlockingQueue que;
+    Cons(ArrayBlockingQueue que){
+        this.que=que;
+    }
+    //consume messages when the queue is not empty
+    @Override
+    public void run(){
+        try{
+            for (int i=0;i<20;i++) {
+                System.out.println("Cons"+i+" consumed "+que.poll());
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
+public class ProdConSingleThread {
+    public static void main(String[] args) throws InterruptedException {
+        ArrayBlockingQueue que = new ArrayBlockingQueue(5);
+        Thread P1 = new Thread(new Prod(que));
+        Thread C1 = new Thread(new Cons(que));
+        Thread C2 = new Thread(new Cons(que));
+        P1.start();
+        C1.start();
+//        System.out.println("at que);
+
+    }
+
+}
+
